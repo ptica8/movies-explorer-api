09a03663141f9c7e49764a6960ca213a8452621f
ptica8 <lera-mikshina@mail.ru>
2022-12-13 19:28:46 +0100

    Update limiter.js
    change request limit

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ const limiter = require('express-rate-limit');
 
 module.exports.rateLimiter = limiter({
   windowMs: 24 * 60 * 60 * 1000,
-  max: 100,
+  max: 2500,
   message: 'Превышено количество запросов за 24 часа с одного IP',
   standardHeaders: true,
   legacyHeaders: false,
